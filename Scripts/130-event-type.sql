e851709bc79439a88d66eaea4225f321df0f3284
Myles Andre <myles.andre@possible.com>
2019-10-27 17:42:36 -0400

    Added a mysql call to remove data from event log

--- Scripts/130-event-type.sql
+++ Scripts/130-event-type.sql
@@ -3,4 +3,6 @@ ADD COLUMN `eventType` INT NOT NULL AFTER `updated`;
 
 ALTER TABLE `applicationdomain`.`events`
     CHANGE COLUMN `original` `original` VARCHAR(3000) NULL DEFAULT NULL ,
-    CHANGE COLUMN `updated` `updated` VARCHAR(3000) NULL DEFAULT NULL ;
+    CHANGE COLUMN `updated` `updated` VARCHAR(3000) NULL DEFAULT NULL;
+
+TRUNCATE `applicationdomain`.`events`;
